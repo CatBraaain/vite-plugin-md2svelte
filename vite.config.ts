8be4a7e6d74a81b9e91f53f3c150ba0f8a487cec
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-05-06 14:45:10 +0900

    chore: add oxfmt/oxlinter config

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,35 @@ export default defineConfig({
       exclude: ["test/**", "**/*.test.ts"],
     },
   },
+  fmt: {
+    sortImports: true,
+    sortPackageJson: {
+      sortScripts: true,
+    },
+  },
+  lint: {
+    plugins: ["unicorn", "typescript", "oxc"],
+    rules: {
+      "unicorn/prefer-node-protocol": "warn",
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          fix: {
+            imports: "off",
+            variables: "off",
+          },
+        },
+      ],
+    },
+    settings: {
+      vitest: {
+        typecheck: false,
+      },
+    },
+    env: {
+      builtin: true,
+    },
+  },
 });
